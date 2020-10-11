9f631c58ab96d83ce5467c84500372d465dc7e52
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-10-11 16:47:33 -0600

    attack methods done

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+const getRandomValue = (max, min) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
 const app = Vue.createApp({
     data() {
         return {
-
+            playerHealth: 150,
+            monsterHealth: 300
         }
     },
     computed: {
 
     },
     methods: {
-        
+        attack() {
+            const attackStr = getRandomValue(15, 5);
+            this.monsterHealth -= attackStr;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackStr = getRandomValue(20, 9);
+            this.playerHealth -= attackStr;
+        }
     }
 });
 app.mount('#game');
