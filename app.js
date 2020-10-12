2da720d712d109e5aeb0005a246cacf5251f0ef4
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-10-11 18:03:49 -0600

    core game functional

--- app.js
+++ app.js
@@ -7,20 +7,51 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         }
     },
     computed: {
         monsterHealthBar() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%'};
+            } else if (this.winner === 'Draw') {
+                return { width: '0%'};
+            }
             return {width: this.monsterHealth + '%'}
         },
         playerHealthBar() {
+            if (this.playerHealthBar < 0) {
+                return { width: '0%'};
+            } else if (this.winner === 'Draw') {
+                return { width: '0%'};
+            }
             return {width: this.playerHealth + '%'}
         },
         useSpecialAttack() {
             return this.currentRound % 3 !==0
         }
 
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0  && this.monsterHealth <= 0) {
+                //Draw
+                this.winner = 'Draw'
+            } else if (value <= 0) {
+                //Player lost
+                this.winner = 'Monster'
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0  && this.playerHealth <= 0) {
+                //Draw
+                this.winner = 'Draw'
+            } else if (value <= 0) {
+                //Monster lost
+                this.winner = 'Player'
+            }
+        }
     },
     methods: {
         attack() {
@@ -42,14 +73,21 @@ const app = Vue.createApp({
         healPlayer() {
             this.currentRound++;
             const healAmount = getRandomValue(20, 10)
-            this.playerHealth += healAmount;
+            if (this.playerHealth + healAmount > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healAmount;
+            }
             this.attackPlayer();
         },
         surrender() {
-            alert("You just lost!");
-            this.counter === 0;
+            this.winner = 'Monster';
+        },
+        playAgain() {
+            this.currentRound = 0;
             this.monsterHealth = 100;
             this.playerHealth = 100;
+            this.winner = null;
         }
     }
 });
